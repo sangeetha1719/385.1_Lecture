b4f898bf4c2a37ad87e87c1a06001045bd3b8c95
Sangeetha Prabu <sangeetha.prabu1191@gmail.com>
2026-04-22 14:38:54 -0400

    INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Python Revision
+-   to run python script `python <filename>` or `python3 <filename>`
+
+## Quick Keys:
+- open and close terminal `ctrl + ~`
+- clearing/cleaning terminal: type `clear` or `ctrl + l`
+- to view previous terminal commands cycle `up-arrow` or `downarrow`
+- toggle comment `ctrl + ?`
+
+## Build-it functions:
+- `print()` - prints values to the console
+- `input()` - gets user input from the console
---
+++ welcome.py
@@ -0,0 +1,26 @@
+# single line comments
+print('Welcome to python!!')
+print('Hello , World !!')
+# """
+# MULTI-LINE comments use Either three or 
+# single or three double quotes
+# """
+print('Per scholas is Unlocking the potential and Changing the Face of Tech')
+
+print (10 + 90)
+print (300.5 + 0.56)
+
+# sep= attribute/parameter puts a separator of choice betwenn the comma separated value
+print (12, 24, -2, sep='**')
+print('but' ,'not','including', sep='**')
+print('but' ,'not','including', sep=' ')
+
+# saving response into variable
+name = input('What is your name?')
+print ('Hello', name , 'what is up?')
+
+# Get numbers from the user
+mb = input('Enter your Mobile Number is :')
+hp = input('Enter your Home Number:')
+print('Mobile number is ',mb)
+print('Home Number is', hp)
