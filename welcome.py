07e771867cd5e0cdc89c72f877accf12892d714a
Sangeetha Prabu <sangeetha.prabu1191@gmail.com>
2026-04-22 15:19:23 -0400

    Added OS modules

--- welcome.py
+++ welcome.py
@@ -1,26 +1,43 @@
-# single line comments
-print('Welcome to python!!')
-print('Hello , World !!')
-# """
-# MULTI-LINE comments use Either three or 
-# single or three double quotes
-# """
-print('Per scholas is Unlocking the potential and Changing the Face of Tech')
+# Using import statement to import a module
+import sys
+import math        # packages gives us access to prebuilt functions
+from math import pi  # importing specific functions from a module
+import os 
+print('Python Version:', sys.version) 
+# print(math.sqrt(16))
+# print(math.pi)
+# print(os.listdir())
+# print(os.listdir('/Volumes/ExtHD'))
+# print(os.getcwd())
+# print(os.environ)
+# print(os.name)
+# print(sys.path)
+print(sys.modules)
+# for path in sys.path:
+#     print(path)
+# # single line comments
+# print('Welcome to python!!')
+# print('Hello , World !!')
+# # """
+# # MULTI-LINE comments use Either three or 
+# # single or three double quotes
+# # """
+# print('Per scholas is Unlocking the potential and Changing the Face of Tech')
 
-print (10 + 90)
-print (300.5 + 0.56)
+# print (10 + 90)
+# print (300.5 + 0.56)
 
-# sep= attribute/parameter puts a separator of choice betwenn the comma separated value
-print (12, 24, -2, sep='**')
-print('but' ,'not','including', sep='**')
-print('but' ,'not','including', sep=' ')
+# # sep= attribute/parameter puts a separator of choice betwenn the comma separated value
+# print (12, 24, -2, sep='**')
+# print('but' ,'not','including', sep='**')
+# print('but' ,'not','including', sep=' ')
 
-# saving response into variable
-name = input('What is your name?')
-print ('Hello', name , 'what is up?')
+# # saving response into variable
+# name = input('What is your name?')
+# print ('Hello', name , 'what is up?')
 
-# Get numbers from the user
-mb = input('Enter your Mobile Number is :')
-hp = input('Enter your Home Number:')
-print('Mobile number is ',mb)
-print('Home Number is', hp)
+# # Get numbers from the user
+# mb = input('Enter your Mobile Number is :')
+# hp = input('Enter your Home Number:')
+# print('Mobile number is ',mb)
+# print('Home Number is', hp)
